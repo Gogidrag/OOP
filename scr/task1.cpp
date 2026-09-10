6be343d8eb905812f71a0f71d85d10fb0d37a455
Dmitry <yndsss@MacBook-Pro-Dmitry.local>
2026-09-10 13:43:43 +0500

    Perviy commit

---
+++ scr/task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+void fillArray(int (&arr)[10]){
+    srand(time(nullptr)); // generator nastroika
+    arr[i] = rand() % 100; // ot 0 do 99
+
+}
+int main(){
+    int arr[10];
+
+    return 0;
+}
