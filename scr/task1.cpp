3a9ab8853ea2c948823fa2459ea47630f542e5ca
Dmitry <yndsss@MacBook-Pro-Dmitry.local>
2026-09-10 14:04:56 +0500

    fillarray

--- scr/task1.cpp
+++ scr/task1.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-void fillArray(int (&arr)[10]){
-    srand(time(nullptr)); // generator nastroika
-    arr[i] = rand() % 100; // ot 0 do 99
-
-}
-int main(){
-    int arr[10];
-
-    return 0;
+void fillArray(int(&arr)[10]){ //int arr 10 ssilka na masiv iz 10 chisel
+    // заполняем массив случайными числами 
+    for (int i = 0; i < 10; i++){
+        arr[i] = rand() % 100;
+    }
 }
