646903b5f64000e79fe8d96313e9290d29602780
Dmitry <yndsss@MacBook-Pro-Dmitry.local>
2026-09-10 14:14:46 +0500

    Вывод на экран массива

--- scr/task1.cpp
+++ scr/task1.cpp
@@ -7,3 +7,11 @@ void fillArray(int(&arr)[10]){ //int arr 10 ssilka na masiv iz 10 chisel
         arr[i] = rand() % 100;
     }
 }
+ // вывод на экран массива 
+void swapElement(int(&arr)[10], int& i1,int& i2){
+    for (auto x : arr){ // каждый элемент проходим
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+}
