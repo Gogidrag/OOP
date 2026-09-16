3bfa44c1899a51d3c8c9822096322577969dcb09
Gogidrag <shigoo@yandex.ru>
2026-09-17 01:51:54 +0500

    main

--- scr/3.cpp
+++ scr/3.cpp
@@ -51,6 +51,46 @@ void reSizeArray(SafeArray& arr, int newSize) {
     arr.data = newData;  // переключаем указатель
     arr.size = newSize;  // обновляем размер
 }
+int main() {
+    cout << "Задание 3\n\n";
+
+    // Создаём массив из 5 элементов
+    SafeArray arr = createArray(5);
+
+    // Заполняем
+    for (int i = 0; i < arr.size; i++) {
+        arr.data[i] = (i + 1) * 10;
+    }
+
+    cout << "Исходный: ";
+    printSafe(arr);
+
+    // Меняем 2 элемент через getElement
+    cout << "\nМеняем элемент с индексом 2 на 999:\n";
+    getElement(arr, 2) = 999;   // ← работает слева от =
+    printSafe(arr);
+
+    // Проверка выхода за границы
+    cout << "\nПроверка выхода за границы (индекс 10):\n";
+    getElement(arr, 10) = 777; 
+    printSafe(arr);
+
+    // Уменьшаем размер
+    cout << "\nУменьшаем размер с 5 до 3:\n";
+    reSizeArray(arr, 3);
+    printSafe(arr);
+
+    // Увеличиваем размер
+    cout << "\nУвеличиваем размер с 3 до 7:\n";
+    reSizeArray(arr, 7);
+    printSafe(arr);
+
+    // Освобождение
+    delete[] arr.data;
+    arr.data = nullptr;
+
+    return 0;
+}
 
 
 
