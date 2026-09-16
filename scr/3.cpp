ef15faacac0c396a3212af8eaf142a773e0f2122
Gogidrag <shigoo@yandex.ru>
2026-09-17 01:12:48 +0500

    SafeArray

---
+++ scr/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct SafeArray {
+    int* data;  
+    int size;   
+};
+SafeArray createArray(int size) {
+    SafeArray arr;
+    arr.size = size;
+    arr.data = new int[size]{};   // {} — обнуляем
+    return arr;
+}
