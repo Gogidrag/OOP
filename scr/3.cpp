30361aff0b35b6712cad6e8b1adfcb3fefbf1a84
Gogidrag <shigoo@yandex.ru>
2026-09-17 01:23:11 +0500

    getelement

--- scr/3.cpp
+++ scr/3.cpp
@@ -11,3 +11,13 @@ SafeArray createArray(int size) {
     arr.data = new int[size]{};   // {} — обнуляем
     return arr;
 }
+int& getElement(SafeArray& arr, int index) {
+    static int dummy = 0; 
+
+    if (index < 0 || index >= arr.size) {
+        cout << "ОШИБКА: индекс " << index
+             << " вне границ [0.." << arr.size - 1 << "]\n";
+        return dummy;
+    }
+    return arr.data[index];
+}
